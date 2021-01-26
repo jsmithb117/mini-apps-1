1083e129f6a74242153c8323af1e571a9526e5b4
jsmithb117 <jsmithb117@gmail.com>
2021-01-25 20:01:38 -0800

    Renders a draw. Model, View, and Controller separated within one file.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,36 +1,19 @@
+/////////-----------------------------View---------------------/////////
 var places = document.getElementsByClassName('place');
+/////////-----------------------------View---------------------/////////
 
-var playerOnesTurn = true;
 
-var checkCRD = (...rows) => { //checks each row/column, or diagonal depending on how many elements are passed in.
-  var rowXcount = rowOcount = 0;
-  for(let row of rows) {
-    row.map((elem) => {
-      rowXcount = (elem === 'X') ? rowXcount + 1 : rowXcount;
-      rowOcount = (elem === 'O') ? rowOcount + 1 : rowOcount;
-    })
-    if (rowXcount === 3) {
-      announceWinner('X');
-    } else if (rowOcount === 3) {
-      announceWinner('O');
-    } else { //reset for next row
-      rowXcount = rowOcount = 0;
-    }
-  }
+
+/////////-----------------------------Controller---------------------/////////
+var clickHandler = (elem) => {
+  var id = elem.srcElement.attributes.id.nodeValue;
+  var xOrO = playerOnesTurn ? 'X' : 'O';
+  var node = document.getElementById(id);
+  node.innerHTML = xOrO; //assigns value to board piece
+  node.className += ` ${xOrO}`; //attaches the value as an additional class, used for 'announceWinner'
+  playerOnesTurn = !playerOnesTurn;
+  checkBoard();
 };
-
-
-var announceWinner = (xOrO) => { //self explanatory
-  var notXorO = xOrO === 'X' ? 'O' : 'X';
-  for (let elem of document.getElementsByClassName(xOrO)) {
-    elem.style.color = 'green';
-  };
-  for (let elem of document.getElementsByClassName(notXorO)) {
-    elem.style.color = 'red';
-  }
-};
-
-
 
 var checkBoard = () => { //Uses checkCRD to check if there is a winner after each turn
   var board = [[], [], []];
@@ -45,24 +28,64 @@ var checkBoard = () => { //Uses checkCRD to check if there is a winner after eac
   var diagOne = [board[0][0], board[1][1], board[2][2]];
   var diagTwo = [board[2][0], board[1][1], board[0][2]];
   checkCRD(diagOne, diagTwo); //checks diagonals
-
 };
 
+var checkCRD = (...rows) => { //checks each row/column, or diagonal depending on how many elements are passed in.
+  var rowXcount = rowOcount = totalCount = 0;
+  for(let row of rows) {
+    // debugger;
+    row.map((elem) => {
+      rowXcount = (elem === 'X') ? rowXcount + 1 : rowXcount;
+      rowOcount = (elem === 'O') ? rowOcount + 1 : rowOcount;
+      totalCount += (elem === 'X' || elem === 'O') ? 1 : 0;
+    })
+    if (rowXcount === 3) {
+      announceWinner('X');
+    } else if (rowOcount === 3) {
+      announceWinner('O');
+    } else if (totalCount === 9) {
+      // debugger;
+      announceDraw();
+    } else { //reset for next row
+      rowXcount = rowOcount = 0;
+    }
+  }
+};
 
-var clickHandler = (elem) => {
-  var id = elem.srcElement.attributes.id.nodeValue;
-  var xOrO = playerOnesTurn ? 'X' : 'O';
-  var node = document.getElementById(id);
-  node.innerHTML = xOrO; //assigns value to board piece
-  node.className += ` ${xOrO}`; //attaches the value as an additional class, used for 'announceWinner'
-  playerOnesTurn = !playerOnesTurn;
-  checkBoard();
+var announceWinner = (xOrO) => { //self explanatory
+  var notXorO = xOrO === 'X' ? 'O' : 'X';
+  for (let elem of document.getElementsByClassName(xOrO)) {
+    elem.style.color = 'green';
+    console.dir(elem);
+  };
+  for (let elem of document.getElementsByClassName(notXorO)) {
+    elem.style.color = 'red';
+  }
+};
+
+var announceDraw = () => {
+  // debugger;
+  for (let elem of document.getElementsByClassName('place')) {
+    elem.style.color = 'indigo';
+  }
 }
 
+var boardReset = () => {
+  for (let elem in places) {
+    elem.innerHTML = '';
+    elem.className = 'place';
+  }
+}
+/////////-----------------------------Controller---------------------/////////
+
+/////////-----------------------------Model---------------------/////////
 var createNewBoard = () => { //only assigns clickHandlers for now
   for (let elem of places) {
     elem.onclick = clickHandler;
   }
 };
 
+var playerOnesTurn = true;
+
 createNewBoard();
+/////////-----------------------------Model---------------------/////////
