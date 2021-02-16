430b467b212d63bc26f205a15a2e84a5eb6284f2
jsmithb117 <jsmithb117@gmail.com>
2021-02-16 08:30:53 -0800

    Express installed and running

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,25 @@
+const express = require('express');
+let app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const port = 3000;
+
+app.use((req, res, next) => {
+  console.log('did stuff');
+  next();
+});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res, next) => {
+  console.log('req.body: ', req.body);
+  res.send('sent');
+})
 
 
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(port, () => {
+  console.log(`App listening on port: ${port}`);
+})
