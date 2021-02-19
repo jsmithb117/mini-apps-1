df97557503d29ef7c3ab9ab5299d4d151d20d8ef
jsmithb117 <jsmithb117@gmail.com>
2021-02-19 14:25:12 -0800

    Checks moved to separate file.  Fixed some inconsistencies in the checks.

--- challenge_4/client/src/components/app.jsx
+++ challenge_4/client/src/components/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Board from './Board.jsx';
 import Display from './Display.jsx';
+import { checkCols, checkDiags, checkRows } from './checks.js';
 
 class App extends React.Component {
   constructor(props) {
@@ -21,6 +22,8 @@ class App extends React.Component {
     }
   };
 
+
+
   swapPlayer(player) {
     var next = player === 'red' ? 'blue' : 'red';
     this.setState({ turn: next });
@@ -39,152 +42,19 @@ class App extends React.Component {
       }
     }
     changed ? this.setState({ board }) : this.setState({ message: 'Invalid move!' });
-    this.checkCols();
-    this.checkRows();
-    this.checkDiags();
-  };
-
-  checkCols() {
-    let winner = '';
-    let board = [...this.state.board];
-    for (let col = 0; col <= 6; col++) { //iterate board by cols
-      let currentPlayer = '';
-      let count = 0;
-      for (let row = 0; row <= 5; row++) {
-        let piece = board[col][row];
-        if (currentPlayer === '' && piece[0] !== 'white') {
-          currentPlayer = piece[0];
-          count++;
-        } else {
-          if (currentPlayer === piece[0]) {
-            count++;
-          } else if (piece[0] !== 'white') {
-            count = 1;
-            currentPlayer = piece[0];
-          }
-          if (count >= 4) {
-            winner = currentPlayer;
-            this.setState({ winner });
-          }
-        }
-      }
-    }
-  };
-
-  checkDiags() {
-    let board = this.state.board;
-    let majorDiags = {};
-    let minorDiags = {};
-    let majorBegins = [[0, 2], [0, 1], [0, 0], [1, 0], [2, 0], [3, 0]];
-    //for [0,2] (major)
-    //[0,2], [1,3], [2,4], 3,5]
-    //for [3,5] (minor)
-    //[3,5], [4,4], [5,3], [6,2]
-    let minorBegins = [[3, 5], [2, 5], [1, 5], [0, 5], [0, 4], [0, 3]];
-
-    for (let i = 0; i <= 5; i++) {
-      //(major, minor)Diags looks like: { 1: {begin: [0,2], currentPlayer: null, count: 0}, ... 5: {begin: [3,0], currentPlayer: null, count: 0}}
-      majorDiags[i] = {
-        begin: majorBegins.shift(),
-        currentPlayer: null,
-        count: 0
-      }
-      minorDiags[i] = {
-        begin: minorBegins.shift(),
-        currentPlayer: null,
-        count: 0
-      }
-    };
-
-    for (let elem in minorDiags) {
-      var minorX = minorDiags[elem].begin[0];
-      var minorY = minorDiags[elem].begin[1];
-
-      while (minorX <= 6 && minorY >= 0) {
-        var minorPiece = board[minorX][minorY];
-
-        if (minorPiece[0] !== 'white') {
-          if (minorDiags[elem].currentPlayer === minorPiece[0]) {
-            minorDiags[elem].count++;
-          } else {
-            minorDiags[elem].currentPlayer = minorPiece[0];
-            minorDiags[elem].count++;
-          }
-        }
-
-        if (minorDiags[elem].count >= 4) {
-          var winner = minorDiags[elem].currentPlayer;
-          this.setState({ winner });
-          break;
-        }
-
-        minorX++;
-        minorY--;
-      }
-    }
-
-    for (let elem in majorDiags) {
-      var majorX = majorDiags[elem].begin[0];
-      var majorY = majorDiags[elem].begin[1];
-
-      while (majorX <= 6 && majorY <= 5) {
-        var majorPiece = board[majorX][majorY];
-
-        if (majorPiece[0] !== 'white') {
-          if (majorDiags[elem].currentPlayer === majorPiece[0]) {
-            majorDiags[elem].count++;
-          } else {
-            majorDiags[elem].currentPlayer = majorPiece[0];
-            majorDiags[elem].count++;
-          }
-        }
-
-        if (majorDiags[elem].count >= 4) {
-          var winner = majorDiags[elem].currentPlayer;
-          this.setState({ winner });
-          break;
-        }
-
-        majorX++;
-        majorY++;
-      }
+    var colsWinner = checkCols(this.state.board);
+    var rowsWinner = checkRows(this.state.board);
+    var diagsWinner = checkDiags(this.state.board);
+    var winner = colsWinner ? colsWinner
+    : rowsWinner ? rowsWinner
+    : diagsWinner ? diagsWinner
+    : null;
+    if (winner) {
+      this.setState({ winner });
     }
   };
 
 
-  checkRows() {
-    let winner;
-    let board = [...this.state.board];
-    let rows = {};
-    for (let i = 0; i <= 6; i++) { //populate rows to look like {0: {currentPlayer: '', count: 0} ... 6: {currentPlayer: '', count: 0}}
-      rows[i] = {
-        currentPlayer: null,
-        count: 0
-      }
-    }
-    for (let i = 0; i < board.length; i++) {
-      if (this.state.winner) {
-        break;
-      }
-      for (let j = 0; j < board[i].length; j++) {
-        let piece = board[i][j];
-        if (piece[0] !== 'white') {
-          if (rows[piece[2]].currentPlayer === piece[0]) {
-            rows[j].count++;
-          } else {
-            rows[j].currentPlayer = piece[0];
-            rows[j].count = 1;
-          }
-          if (rows[j].count >= 4) {
-            winner = rows[j].currentPlayer;
-            this.setState({ winner });
-            console.log('board: ', board);
-            break;
-          }
-        }
-      }
-    }
-  };
 
   render() {
     return (
---
+++ challenge_4/client/src/components/checks.js
@@ -0,0 +1,125 @@
+const checkCols = (board) => {
+  let winner;
+  for (let col = 0; col <= 6; col++) { //iterate board by cols
+    let currentPlayer;
+    let count = 0;
+    for (let row = 0; row <= 5; row++) {
+      let piece = board[col][row];
+      if (piece[0] === currentPlayer) {
+        count++;
+      } else {
+        currentPlayer = piece[0];
+        count = 1;
+      }
+      if (count >= 4 && currentPlayer !== 'white') {
+        winner = currentPlayer;
+        return winner;
+      }
+    }
+  }
+  return winner;
+};
+
+const checkDiags = (board) => {
+  let winner;
+  let majorDiags = {};
+  let minorDiags = {};
+  let majorBegins = [[0, 2], [0, 1], [0, 0], [1, 0], [2, 0], [3, 0]];
+  let minorBegins = [[3, 5], [2, 5], [1, 5], [0, 5], [0, 4], [0, 3]];
+
+  for (let i = 0; i <= 5; i++) {
+    //(major, minor)Diags looks like: { 1: {begin: [0,2], currentPlayer: null, count: 0}, ... 5: {begin: [3,0], currentPlayer: null, count: 0}}
+    majorDiags[i] = {
+      begin: majorBegins.shift(),
+      currentPlayer: null,
+      count: 0
+    }
+    minorDiags[i] = {
+      begin: minorBegins.shift(),
+      currentPlayer: null,
+      count: 0
+    }
+  };
+
+  for (let elem in minorDiags) {
+    var minorX = minorDiags[elem].begin[0];
+    var minorY = minorDiags[elem].begin[1];
+
+    while (minorX <= 6 && minorY >= 0) {
+      var minorPiece = board[minorX][minorY];
+
+      if (minorDiags[elem].currentPlayer === minorPiece[0]) {
+        minorDiags[elem].count++;
+      } else {
+        minorDiags[elem].currentPlayer = minorPiece[0];
+        minorDiags[elem].count = 1;
+      }
+
+
+      if (minorDiags[elem].count >= 4 && minorDiags[elem].currentPlayer !== 'white') {
+        winner = minorDiags[elem].currentPlayer;
+        return winner;
+      }
+
+      minorX++;
+      minorY--;
+    }
+  }
+
+  for (let elem in majorDiags) {
+    var majorX = majorDiags[elem].begin[0];
+    var majorY = majorDiags[elem].begin[1];
+
+    while (majorX <= 6 && majorY <= 5) {
+      var majorPiece = board[majorX][majorY];
+
+      if (majorDiags[elem].currentPlayer === majorPiece[0]) {
+        majorDiags[elem].count++;
+      } else {
+        majorDiags[elem].currentPlayer = majorPiece[0];
+        majorDiags[elem].count = 1;
+      }
+
+      if (majorDiags[elem].count >= 4 && majorDiags[elem].currentPlayer !== 'white') {
+        winner = majorDiags[elem].currentPlayer;
+        return winner;
+        break;
+      }
+
+      majorX++;
+      majorY++;
+    }
+  }
+  return winner;
+};
+
+
+const checkRows = (board) => {
+  let winner;
+  let rows = {};
+  for (let i = 0; i < 6; i++) { //populate rows to look like {0: {currentPlayer: '', count: 0} ... 6: {currentPlayer: '', count: 0}}
+    rows[i] = {
+      currentPlayer: null,
+      count: 0
+    }
+  }
+  for (let i = 0; i < board.length; i++) { //iterate cols
+    for (let j = 0; j < board[i].length; j++) { //iterate rows
+      let piece = board[i][j]; //['white', col, row]
+      if (rows[j].currentPlayer === piece[0]) {
+        rows[j].count++;
+      } else {
+        rows[j].currentPlayer = piece[0];
+        rows[j].count = 1;
+      }
+
+      if (rows[j].count >= 4 && rows[j].currentPlayer !== 'white') {
+        winner = rows[j].currentPlayer;
+        return winner;
+      }
+    }
+  }
+  return winner;
+};
+
+export { checkCols, checkDiags, checkRows };
